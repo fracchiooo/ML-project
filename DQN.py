6efd478a2e9fe8ebc63c2c5ea82756fe139a5e14
Fracchio <sconocchiapisoni.1889241@studenti.uniroma1.it>
2025-03-13 18:13:11 +0100

    fix1

--- DQN.py
+++ DQN.py
@@ -88,9 +88,9 @@ class FrozenLakeAgent:
             target_function = self.model.predict(tf.convert_to_tensor([[state]], dtype=tf.int32), verbose=0) # *Q(s,a), Q[0] = Q(s,a), Q[0][a] = reward for doing the action a from state s
 
             if not terminated:
-                target += self.gamma * (max(self.model.predict(tf.convert_to_tensor([[new_state]], dtype=tf.int32), verbose=0)[0]) - target_function[0][action])  # (r + gamma * (max(Q(s',a')) - Q(s,a)))
+                target += self.gamma * (max(self.model.predict(tf.convert_to_tensor([[new_state]], dtype=tf.int32), verbose=0)[0])) #- target_function[0][action])  # (r + gamma * (max(Q(s',a')) - Q(s,a)))
 
-            target_function[0][action] += self.learning_rate*target  # Q(s,a) = Q(s,a) + alpha * (r + gamma * (max(Q(s',a')) - Q(s,a)))
+            target_function[0][action] = target # + target_function[0][action]) * self.learning_rate     # Q(s,a) = Q(s,a) + alpha * (r + gamma * (max(Q(s',a')) - Q(s,a)))
             self.model.fit(tf.convert_to_tensor([[state]], dtype=tf.int32), target_function, epochs=1, verbose=0)
 
         
